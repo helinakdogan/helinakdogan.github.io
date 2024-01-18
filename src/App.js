09504094c5c8e623aefb9c7dabb4e26f58fdb18b
Helin Akdoğan <akdoganhelin@gmail.com>
2024-01-19 02:20:01 +0300

    linechart

--- src/App.js
+++ src/App.js
@@ -1,37 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 
-
-
-import { UserData } from "./Data";
 import LineChart from "./LineChart";
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
-    datasets: [
-      {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-
   return (
     <div className="App">
-
       <div style={{ width: 700 }}>
-        <LineChart chartData={userData} />
+        <LineChart />
       </div>
     </div>
   );
